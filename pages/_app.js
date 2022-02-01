ba6cf334979dca66d659bcbe7614dd59f31f9cb2
Rokit <bojanforgaming@hotmail.com>
2022-02-01 15:16:25 +0100

    s

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,17 @@ import '../styles/globals.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Bottom from '../components/Footer/Bottom';
+import { useState } from 'react';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }) {
-  return (
+  const [loaded, setLoaded] = useState(false);
+
+  const loader = setTimeout(() => {
+    setLoaded(true);
+  }, 5000);
+
+  return loaded ? (
     <div className="App">
       <div className="scroll">
         <div className="z-10 ">
@@ -16,6 +24,20 @@ function MyApp({ Component, pageProps }) {
           <Bottom />
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="transition-all duration-300 w-[100%] h-[100vh] flex items-center justify-center bg-black flex-col">
+      <div className="relative w-[450px] h-[80px]">
+        <Image
+          alt="logo"
+          src="/images/logo-retina.png"
+          className="transition animate-pulse duration-300"
+          layout="fill"
+          priority
+          objectFit="contain"
+        />
+      </div>
+      <div className="h-[5px] w-[450px]  bg-white rounded-full mt-[25px] animated-line origin-left"></div>
     </div>
   );
 }
